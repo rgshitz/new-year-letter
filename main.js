cd47863d5101c6b0af40539a84a720f252cb79ac
rgshitz <131843067+rgshitz@users.noreply.github.com>
2025-01-01 11:36:28 +0530

    new

---
+++ main.js
@@ -0,0 +1,18 @@
+// Loader
+document.addEventListener("DOMContentLoaded", () => {
+    // Simulate an API request or any async operation
+    setTimeout(() => {
+        hideLoader();
+        showContent();
+    }, 2000); // Replace with your actual data loading logic and time
+
+    function hideLoader() {
+        const loader = document.getElementById("loader");
+        loader.style.display = "none";
+    }
+
+    function showContent() {
+        const content = document.getElementById("content");
+        content.style.display = "block";
+    }
+});
